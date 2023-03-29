bb97ac395521e225c5121eb7c478b7d34088f8e7
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-29 11:14:50 +0300

    Update What is Rest.kt

--- What is Rest.kt
+++ What is Rest.kt
@@ -1,17 +1,24 @@
 
 
-
+When you send a reuest to a server , be it to add or delete something , what you get from the server is what we call a RESOURCE
 
 
 
 
 REST ENABLES US TO PERFORM THE CRUD OPERATIONS BY USING THE HTTP METHODS
+               Http Methods
 
 C  -> Create   - POST
 R  -> Read     - GET
 U  -> Update   - PUT
 D  -> Delete   - DELETE
 
+
+HOW TO IMPLMENT RESTFUL WEBSERVICES
+
+ There are two methods to implement restful webservices 
+1. Jersey
+2. Sparing
 
 
 
