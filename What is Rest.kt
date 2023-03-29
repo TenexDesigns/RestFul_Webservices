74f3623c7aaaf31533239a6ab9cfe8371ef02e30
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-29 10:38:31 +0300

    Create What is Rest.kt

---
+++ What is Rest.kt
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+REST ENABLES US TO PERFORM THE CRUD OPERATIONS BY USING THE HTTP METHODS
+
+C  -> Create   - POST
+R  -> Read     - GET
+U  -> Update   - PUT
+D  -> Delete   - DELETE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
