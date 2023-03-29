f6c0ec9bccc3b7056efc242281d4fab39bb1ca3c
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-29 12:20:28 +0300

    Create RestApi_1.kt

---
+++ RestApi_1.kt
@@ -0,0 +1,59 @@
+WHAT IS REST API
+
+API
+Application Programming Interface
+
+REST
+REpresentational State Transfer
+
+
+REST
+It is an architectural style of the web
+
+It is a standard / set of guidlines by which we can  structure and create API's
+
+
+RESP PROPERTIES
+
+1. Rest api's make use of result based URLs
+
+      // Thses two are simply get requests
+ g.g http://food.com/items -> can return a web pages
+    http://food.com/api/items -> Can return a json object of data consumed in the above wbpage
+    
+2. They make use of HTTP methods 
+
+GET -Used to retreve data from the server
+POST - Used to send data to the server
+PUT -Used to update data
+DELETE -Used to delete data.
+
+3. They make use of HTTP status codes
+EXAMPLES
+-200 means OK
+-404 means resource not found
+-500 means server error. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
