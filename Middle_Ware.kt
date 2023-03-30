349eb4414dc5885b9cf0fcc8e13b79b2105ea8d0
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-30 09:43:48 +0300

    Create Middle_Ware.kt

---
+++ Middle_Ware.kt
@@ -0,0 +1,64 @@
+
+
+REQUEST_______________________________________________
+                                              |
+                                              |app.use(body-parser.json())// This middle ware takes the data sent ,out of the body of the request 
+                                              |
+                                              |
+                                              | Any code that is excuted between the request and response is the middle ware.We apply middle ware by using app.use method
+                                              |
+                                       (express receives request and gives back the response)
+                                              |
+                                              |
+                                              |
+                                              |
+RESPONSE______________________________________________
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
