ed1a1f2368017537b8ce8c6ae7d1ae07f9641315
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-31 07:58:44 +0300

    Create Mongo_DB.kt

---
+++ Mongo_DB.kt
@@ -0,0 +1,19 @@
+In MongoDB, a collection is a grouping of individual documents that have similar characteristics. 
+It is the equivalent of a table in a relational database. Collections are dynamic,
+which means that they can have varying fields and structure from one document to another.
+
+A field is a piece of data stored within a document in a collection.
+It is similar to a column in a relational database table. Fields in MongoDB can store various types of data
+, such as strings, numbers, booleans, dates, and arrays. Unlike relational databases,
+MongoDB does not require a fixed schema or structure for its collections and fields,
+which allows for greater flexibility in storing and retrieving data.
+
+A record in MongoDB is a document that contains one or more fields.
+It is the equivalent of a row in a relational database table. Each record in a MongoDB collection can have a unique set of fields and values, 
+depending on the specific data being stored.
+
+Overall, MongoDB is a document-oriented NoSQL database that uses collections to group related documents, 
+fields to store data within those documents, and records to represent individual documents within a collection.
+
+
+
