1b8df10adbd58fd02c33f401eb85bf2378312bad
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-31 08:20:44 +0300

    Update Mongo_db_Models.kt

--- Mongo_db_Models.kt
+++ Mongo_db_Models.kt
@@ -2,6 +2,33 @@ MODELS
 
 -Models represent collections in Mongo Db
 -Imagine we have a collections of users, we would create a model to represnet that collection of users and that model would be callled the user model.
+In the above case we would have a user model to represent a collection of users
+
+
+A collection is like a table in sql . It represent a series of data.
+
+
+Models work hand in hand with schemas
+
+Schemas define the structure of our data objects in our collection.
+
+e.g
+
+{
+  name:String,
+  rank:String,
+  availability:Boolean
+  
+
+
+}
+
+
+THEN WE ARE GOING TO INSTALL MOOONGOOSE
+
+Mongoose add a layer of methods to eaily save ,edit ,retrieve and delete data from mongodg
+Mongoose allows us to create our models and schemas easily.
+We install mongoose in our project usin gnpm.
 
 
 
